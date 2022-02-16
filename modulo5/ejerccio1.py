4d9561ff2fef118960e0f918dd4c28d617463b7b
Alex Yazmin Manzo Olguin <ada_hack418@hackademy.mx>
2022-02-16 14:27:55 -0600

    se agregan los  katas faltan 3

--- modulo5/ejerccio1.py
+++ modulo5/ejerccio1.py
@@ -6,3 +6,12 @@ print(distancia)
 
 distanciamillas=distancia*0.621
 print(distanciamillas)
+
+planetauno=float(input("ingrse el primer planeta"))
+planetados=float(input("ingrse el segundo planeta"))
+
+distancias=planetados-planetauno
+print(distancias)
+
+distanciamillas1=distancia*0.621
+print(distanciamillas1)
--- modulo6/minymax.py
+++ modulo6/minymax.py
@@ -2,5 +2,6 @@ gravity_on_planets = [0.378, 0.907, 1, 0.379, 2.36, 0.916, 0.889, 1.12]
 bus_weight = 12650 # in kilograms, on Earth
 
 print('On Earth, a double-decker bus weighs', bus_weight, 'kg')
+print('On Mercury, a double-decker bus weighs', bus_weight * gravity_on_planets[0], 'kg')
 print('The lightest a bus would be in the solar system is', bus_weight * min(gravity_on_planets), 'kg')
 print('The heaviest a bus would be in the solar system is', bus_weight * max(gravity_on_planets), 'kg')
